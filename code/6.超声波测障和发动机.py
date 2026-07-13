3cd9b84db69254a225ef87c2ce1b489307994c86
隔壁老王 <1438426721@qq.com>
2026-07-14 00:00:11 +0800

    Motor 类接口设计

--- code/6.超声波测障和发动机.py
+++ code/6.超声波测障和发动机.py
@@ -1,4 +1,4 @@
-from machine import Pin
+from machine import Pin,PWM
 FORWARD  =  1
 BACKWARD = 2
 STOP = 3
@@ -6,29 +6,35 @@ class Motor:
     def __init__(self,pin_num_1,pin_num_2,pin_num_3) :
         self.AIN1 = Pin(pin_num_1,Pin.OUT)
         self.AIN2 = Pin(pin_num_2,Pin.OUT)
-        self.PWMA = PWM(pin_num_3,Pin.OUT) 
+        self.PWMA = PWM(Pin(pin_num_3),freq = 1000, duty_u16 = 0) 
         self.speed = 0
         self.direction = 0
         self.is_running = 0
     def forward(self,speed):
         self.AIN1(1)
         self.AIN2(0)
-        self.PWMA = 
-        self.speed = speed
+        self.set_speed(speed)
         self.direction = FORWARD
         self.is_running = True
     def backward(self,speed):
         self.AIN1(0)
-        self.AIN2(1)
-        self.PWMA = 
-        self.speed = speed
+        self.AIN2(1) 
+        self.set_speed(speed)
         self.direction = BACKWARD
         self.is_running = True
     def stop(self):
         self.AIN1(0)
         self.AIN2(0)
+        self.set_speed(0)
         self.direction = STOP
         self.is_running = False
-    def pwm(self):
-        self.PWMA()
+    def set_speed(self,speed):
+        if speed  > 100:
+            speed = 100
+        elif speed < 0 :
+            speed = 0
+        if speed == self.speed :
+            return
+        self.speed = speed
+        self.PWMA.duty_u16(int(speed/100*65535))
     
