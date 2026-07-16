1a75a887d20ccee295f2e4229ad5a482dd6d6d5e
隔壁老王 <1438426721@qq.com>
2026-07-17 00:38:19 +0800

    motor测试实验

---
+++ experiments/11.Motor_test.py
@@ -0,0 +1,21 @@
+from motor import Motor
+import time
+motor = Motor(1,2,3)
+def test_speed(speed,test_name):
+    motor.set_speed(speed)
+    time.sleep(2)
+    assert motor.speed == speed
+    if speed == 0 :
+        assert motor.is_running == False
+    else:
+        assert motor.is_running == True
+    print(test_name,"通过")
+def test_limit(input_speed,expected_speed,test_name):
+    motor.set_speed(input_speed)
+    time.sleep(2)
+    assert motor.speed == expected_speed
+    print(test_name,"通过")
+test_speed(50,"正转测试")
+test_speed(-50,"反转测试")
+test_limit(200,100,"边界测试")
+motor.stop()
