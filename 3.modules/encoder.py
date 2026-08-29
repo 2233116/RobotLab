c425fe64012485da3aed44227f31f9676ab46d16
隔壁老王 <1438426721@qq.com>
2026-08-29 21:31:31 +0800

    .

--- 3.modules/encoder.py
+++ 3.modules/encoder.py
@@ -1,12 +1,18 @@
 class Encoder:
-
-
-
+    def __init__(self,pin_num_A,pin_num_B,counts_per_rev,gear_ratio):
+        self.pin_A = Pin(pin_num_A,Pin.IN,Pin.PULL_UP)
+        self.pin_B = Pin(pin_num_B,Pin.IN,Pin.PULL_UP)
+        self.counts_per_rev = counts_per_rev
+        self.gear_ratio = gear_ratio
+        self.previous_A = None
+        self.start_time = None
+        self.pulse_count = 0
+        self.current_speed = 0
 
     def on_pulse(self):
         current_A = self.pin_A.value()
         current_B = self.pin_B.value()
-        if self.previous_A == None:
+        if self.previous_A is None:
             self.previous_A = current_A
         else:
             if  current_A == 1 :
@@ -21,4 +27,21 @@ class Encoder:
                     self.pulse_count -= 1
         self.previous_A = current_A
 
-            
+    def update(self):
+        self.current_time = time.time()
+        if self.start_time is None:
+            self.start_time = self.current_time
+            return
+        else:     
+            self.elapsed_time = self.current_time - self.start_time
+            if self.elapsed_time >= 0.2 :
+                irq_state = machine.disable_irq()
+                local_count =  self.pulse_count
+                self.pulse_count = 0
+                machine.enable_irq(irq_state)
+                self.current_speed = local_count/self.counts_per_rev/self.elapsed_time*60/self.gear_ratio
+                self.start_time = self.current_time
+            else:
+                return
+    def get_current_speed(self):
+        return self.current_speed
